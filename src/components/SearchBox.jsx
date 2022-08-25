a57c9a267eb74c6f3b04752ac77eb7f87e7aafde
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-25 15:44:44 +0300

    Update SearchBox.jsx

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+const SearchBox = ({ value, onSubmit, onChange }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBox;
