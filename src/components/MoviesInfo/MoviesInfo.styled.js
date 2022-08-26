09b74690fb2bb965e1d1c0eabbcc874eb7acb422
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-26 12:19:59 +0300

    Create MoviesInfo.styled.js

---
+++ src/components/MoviesInfo/MoviesInfo.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const MovieInfoWrap = styled.div`
+  display: flex;
+  padding-top: 20px;
+  // padding-left: 20px;
+`;
+
+export const MovieInfoPoster = styled.img`
+  display: block;
+  // width: 250px;
+  // height: 350px;
+  margin-right: 25px;
+`;
+export const MovieOverview = styled.div`
+  display: block;
+  width: 1000px;
+`;
