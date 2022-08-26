7aea46fbafd87b9d0be672212768ef156fe56896
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-26 17:08:01 +0300

    Update CastList.jsx

--- src/components/CastList/CastList.jsx
+++ src/components/CastList/CastList.jsx
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
-
+import Nophoto from '../../images/Nophoto.png';
 const CastList = ({ cast }) => {
   return (
     <ul className="castList">
       {cast.slice(0, 10).map(({ id, name, character, profile_path }) => (
         <li className="castListItem" key={id}>
-          <img
-            className="castListItemImage"
-            src={`https://image.tmdb.org/t/p/original/${profile_path}`}
-            alt={name}
-            width="100"
-          />
+          {profile_path ? (
+            <img
+              className="castListItemImage"
+              src={`https://image.tmdb.org/t/p/original/${profile_path}`}
+              alt={name}
+              width="100"
+            />
+          ) : (
+            <img src={Nophoto} alt="No_image_poster" width="200" />
+          )}
           <p>{name}</p>
           <p>Character: {character}</p>
         </li>
